f0a333b0b8c5dc3a9671ebbc1277b8f001ea49d9
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2023-02-02 00:41:40 +0000

    adding new feature, remove item and fixed bug such as closing tab and reload page

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,16 @@
-let refe = [];
-let checkJunior = document.getElementById("junior");
-let checkBaby = document.getElementById("baby");
+const storedRefe = JSON.parse(localStorage.getItem("Referencias")) || [];
+let refe = [...storedRefe];
+
+const checkJunior = document.getElementById("junior");
+const checkBaby = document.getElementById("baby");
+const remove = document.getElementById("del_last");
 
 document.addEventListener(
 	"keypress",
 	function (e) {
 		if (e.which === 13) {
 			shown();
+			displayReferences();
 		}
 	},
 	false
@@ -21,6 +25,7 @@ const shown = () => {
 		user = userInput + " (junior)";
 		refe.push(user);
 		action();
+		displayReferences();
 	} else if (checkBaby.checked) {
 		user = userInput + " (baby)";
 		refe.push(user);
@@ -45,7 +50,19 @@ const displayReferences = () => {
 	const storedRefe = JSON.parse(localStorage.getItem("Referencias")) || [];
 
 	let text = "";
-	console.log("referencias", storedRefe);
+	storedRefe.sort((a, b) => a.localeCompare(b));
+	let upper = storedRefe.map((e) => e.toUpperCase());
+
+	upper.forEach((item) => {
+		text += item + "<br>";
+	});
+
+	document.getElementById("aparecer").innerHTML = text;
+};
+const displayStorage = () => {
+	const storedRefe = JSON.parse(localStorage.getItem("Referencias")) || [];
+
+	let text = "";
 	storedRefe.sort((a, b) => a.localeCompare(b));
 	let upper = storedRefe.map((e) => e.toUpperCase());
 
@@ -60,6 +77,18 @@ const del = () => {
 	refe = [];
 	localStorage.removeItem("Referencias");
 	document.getElementById("aparecer").innerHTML = "";
+};
+
+const removeLast = () => {
+	let text = "";
+	refe.sort((a, b) => a.localeCompare(b));
+	let upper = refe.map((e) => e.toUpperCase());
+	upper.forEach((item) => {
+		text += item + "<br>";
+	});
+	refe.pop();
+
+	document.getElementById("aparecer").innerHTML = text;
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
