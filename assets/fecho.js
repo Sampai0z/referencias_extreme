5a5fe57ee2f7d062a9283aca657e05e081fce032
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2024-01-01 20:54:59 +0000

    adicionei a opção de colocar o fundo de caixa, para que quando for calcular ja ter o valor do fundo e apenas subtrair para saber o total do dia

--- assets/fecho.js
+++ assets/fecho.js
@@ -3,10 +3,13 @@
 const valorTotal = document.getElementById("total");
 const notas = document.querySelectorAll(".notas_input");
 const moedas = document.querySelectorAll(".coin_input");
+const totalFundo = document.getElementById("totalFundo");
 
 let total = 0;
 
 const calcular = () => {
+  const valorfundoCaixa = document.getElementById("fundo");
+
   moedas.forEach((moeda) => {
     let valorUnitario = parseFloat(moeda.getAttribute("data-valor")) || 0;
     let quantidade = parseFloat(moeda.value) || 0;
@@ -20,6 +23,8 @@ const calcular = () => {
   });
 
   valorTotal.innerHTML = total.toFixed(2);
+  let soma = total.toFixed(2) - parseFloat(valorfundoCaixa.value) || 0;
+  totalFundo.innerHTML = soma.toFixed(2);
 };
 
 const limpar = () => {
@@ -31,6 +36,7 @@ const limpar = () => {
   });
 
   valorTotal.innerHTML = "0.00";
+  totalFundo.innerHTML = "0.00";
 };
 
 const limparInput = () => {
@@ -44,4 +50,5 @@ const limparInput = () => {
 
 const limparTotal = () => {
   valorTotal.innerHTML = "0.00";
+  totalFundo.innerHTML = "0.00";
 };
