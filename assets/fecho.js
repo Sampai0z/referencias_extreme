59008f46288f42430ec9ad423c7ca941ea3972a6
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2024-01-01 20:08:14 +0000

    criado a logica e a parte de soma das moedas

--- assets/fecho.js
+++ assets/fecho.js
@@ -1,1 +1,22 @@
-console.log("foi");
+// console.log("foi");
+
+const notas = document.getElementById("notas").value;
+const valorTotal = document.getElementById("total");
+
+let total = 0;
+
+const calcular = () => {
+  const moedas = document.querySelectorAll(".coin_input");
+  moedas.forEach((moeda) => {
+    let valorUnitario = parseFloat(moeda.getAttribute("data-valor")) || 0;
+    let quantidade = parseFloat(moeda.value) || 0;
+    total += quantidade * valorUnitario;
+  });
+  valorTotal.innerHTML = total.toFixed(2);
+
+  moedas.value = 0;
+};
+
+const limpar = () => {
+  console.log("clear");
+};
