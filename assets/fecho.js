bb86d103036faed5171cd19f4d2e672a1f426d1c
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2024-01-01 20:29:11 +0000

    finalizado, criado botão para calcular, apagar os inputs e/ou apagar os inputs e o total escrito + a estilização dos botões

--- assets/fecho.js
+++ assets/fecho.js
@@ -1,13 +1,12 @@
 // console.log("foi");
 
 const valorTotal = document.getElementById("total");
+const notas = document.querySelectorAll(".notas_input");
+const moedas = document.querySelectorAll(".coin_input");
 
 let total = 0;
 
 const calcular = () => {
-  const moedas = document.querySelectorAll(".coin_input");
-  const notas = document.querySelectorAll(".notas_input");
-
   moedas.forEach((moeda) => {
     let valorUnitario = parseFloat(moeda.getAttribute("data-valor")) || 0;
     let quantidade = parseFloat(moeda.value) || 0;
@@ -24,19 +23,25 @@ const calcular = () => {
 };
 
 const limpar = () => {
-  const moedas = document.querySelectorAll(".coin_input");
-
   moedas.forEach((moeda) => {
     moeda.value = "";
+  });
+  notas.forEach((nota) => {
+    nota.value = "";
   });
 
   valorTotal.innerHTML = "0.00";
 };
 
 const limparInput = () => {
-  const moedas = document.querySelectorAll(".coin_input");
-
+  notas.forEach((nota) => {
+    nota.value = "";
+  });
   moedas.forEach((moeda) => {
     moeda.value = "";
   });
+};
+
+const limparTotal = () => {
+  valorTotal.innerHTML = "0.00";
 };
