0e237a4a0c307bd9b4342435ce3fb0f0d403af7c
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2024-01-01 20:19:28 +0000

    criado a logica e a soma das notas

--- assets/fecho.js
+++ assets/fecho.js
@@ -1,22 +1,42 @@
 // console.log("foi");
 
-const notas = document.getElementById("notas").value;
 const valorTotal = document.getElementById("total");
 
 let total = 0;
 
 const calcular = () => {
   const moedas = document.querySelectorAll(".coin_input");
+  const notas = document.querySelectorAll(".notas_input");
+
   moedas.forEach((moeda) => {
     let valorUnitario = parseFloat(moeda.getAttribute("data-valor")) || 0;
     let quantidade = parseFloat(moeda.value) || 0;
     total += quantidade * valorUnitario;
   });
-  valorTotal.innerHTML = total.toFixed(2);
 
-  moedas.value = 0;
+  notas.forEach((nota) => {
+    let valorUnitario = parseFloat(nota.getAttribute("data-valor")) || 0;
+    let quantidade = parseFloat(nota.value) || 0;
+    total += quantidade * valorUnitario;
+  });
+
+  valorTotal.innerHTML = total.toFixed(2);
 };
 
 const limpar = () => {
-  console.log("clear");
+  const moedas = document.querySelectorAll(".coin_input");
+
+  moedas.forEach((moeda) => {
+    moeda.value = "";
+  });
+
+  valorTotal.innerHTML = "0.00";
+};
+
+const limparInput = () => {
+  const moedas = document.querySelectorAll(".coin_input");
+
+  moedas.forEach((moeda) => {
+    moeda.value = "";
+  });
 };
