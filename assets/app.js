35f425e486bc254eecc10003aed80ab871c93761
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2024-01-10 00:26:50 +0000

    adding a footer and some links to my personal portfolio and github source code of the project

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,16 @@
-let refe = JSON.parse(localStorage.getItem("Referencias"));
+const storedRefe = JSON.parse(localStorage.getItem("Referencias")) || [];
+let refe = [...storedRefe];
+
+const checkJunior = document.getElementById("junior");
+const checkBaby = document.getElementById("baby");
+const remove = document.getElementById("del_last");
 
 document.addEventListener(
   "keypress",
   function (e) {
     if (e.which === 13) {
       shown();
+      displayReferences();
     }
   },
   false
@@ -12,15 +18,29 @@ document.addEventListener(
 
 const shown = () => {
   let userInput = document.getElementById("test").value.trim();
-
   if (!userInput) {
-    alert("Please enter a reference!");
+    alert("Por favor insira uma referencia!");
+  } else if (refe.includes(userInput)) {
+    alert("Essa referencia ja existe");
+  } else if (checkJunior.checked) {
+    user = userInput + " (junior)";
+    refe.push(user);
+    action();
+    displayReferences();
+  } else if (checkBaby.checked) {
+    user = userInput + " (baby)";
+    refe.push(user);
+    action();
   } else {
     refe.push(userInput);
-    clearInput();
-    localStorage.setItem("Referencias", JSON.stringify(refe));
-    displayReferences();
+    action();
   }
+};
+
+const action = () => {
+  clearInput();
+  localStorage.setItem("Referencias", JSON.stringify(refe));
+  displayReferences();
 };
 
 const clearInput = () => {
@@ -31,7 +51,19 @@ const displayReferences = () => {
   const storedRefe = JSON.parse(localStorage.getItem("Referencias")) || [];
 
   let text = "";
-  console.log("referencias", storedRefe);
+  storedRefe.sort((a, b) => a.localeCompare(b));
+  let upper = storedRefe.map((e) => e.toUpperCase());
+
+  upper.forEach((item) => {
+    text += item + "<br>";
+  });
+
+  document.getElementById("aparecer").innerHTML = text;
+};
+const displayStorage = () => {
+  const storedRefe = JSON.parse(localStorage.getItem("Referencias")) || [];
+
+  let text = "";
   storedRefe.sort((a, b) => a.localeCompare(b));
   let upper = storedRefe.map((e) => e.toUpperCase());
 
@@ -47,3 +79,19 @@ const del = () => {
   localStorage.removeItem("Referencias");
   document.getElementById("aparecer").innerHTML = "";
 };
+
+const removeLast = () => {
+  let text = "";
+  refe.sort((a, b) => a.localeCompare(b));
+  let upper = refe.map((e) => e.toUpperCase());
+  upper.forEach((item) => {
+    text += item + "<br>";
+  });
+  refe.pop();
+
+  document.getElementById("aparecer").innerHTML = text;
+};
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  displayReferences();
+});
